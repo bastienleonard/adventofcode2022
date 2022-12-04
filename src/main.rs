f0941bc8f4efcf3166d7ce47fa973274f5a01a25
Bastien Léonard <bastien.leonard@gmail.com>
2022-12-04 21:25:39 +0100

    Improve day 2 readability

--- src/main.rs
+++ src/main.rs
@@ -65,12 +65,32 @@ fn ex1_2() -> i32 {
     biggest3.iter().sum()
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Choice {
     Rock,
     Paper,
     Scissors
 }
 
+impl Choice {
+    fn winner_against(choice: Self) -> Self {
+        match choice {
+            Choice::Rock => Choice::Paper,
+            Choice::Paper => Choice::Scissors,
+            Choice::Scissors => Choice::Rock
+        }
+    }
+
+    fn loser_against(choice: Self) -> Self {
+        match choice {
+            Choice::Rock => Choice::Scissors,
+            Choice::Paper => Choice::Rock,
+            Choice::Scissors => Choice::Paper
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 struct Prediction {
     his_choice: Choice,
     my_choice: Choice
@@ -93,25 +113,13 @@ impl GameResult {
 }
 
 fn result_points(prediction: &Prediction) -> i32 {
-    let result = match prediction.my_choice {
-        Choice::Rock => match prediction.his_choice {
-            Choice::Rock => GameResult::Draw,
-            Choice::Paper => GameResult::Loss,
-            Choice::Scissors => GameResult::Win
-        },
-        Choice::Paper => match prediction.his_choice {
-            Choice::Rock => GameResult::Win,
-            Choice::Paper => GameResult::Draw,
-            Choice::Scissors => GameResult::Loss
-        },
-        Choice::Scissors => match prediction.his_choice {
-            Choice::Rock => GameResult::Loss,
-            Choice::Paper => GameResult::Win,
-            Choice::Scissors => GameResult::Draw
-        }
-    };
-
-    result.points()
+    if prediction.my_choice == prediction.his_choice {
+        GameResult::Draw
+    } else if prediction.my_choice == Choice::winner_against(prediction.his_choice) {
+        GameResult::Win
+    } else {
+        GameResult::Loss
+    }.points()
 }
 
 fn choice_points(choice: &Choice) -> i32 {
@@ -170,16 +178,8 @@ fn ex2_2() -> i32 {
                 _ => unreachable!()
             };
             let my_choice = match game_result {
-                GameResult::Win => match his_choice {
-                    Choice::Rock => Choice::Paper,
-                    Choice::Paper => Choice::Scissors,
-                    Choice::Scissors => Choice::Rock
-                },
-                GameResult::Loss => match his_choice {
-                    Choice::Rock => Choice::Scissors,
-                    Choice::Paper => Choice::Rock,
-                    Choice::Scissors => Choice::Paper
-                },
+                GameResult::Win => Choice::winner_against(his_choice),
+                GameResult::Loss => Choice::loser_against(his_choice),
                 GameResult::Draw => his_choice
             };
             game_result.points() + choice_points(&my_choice)
