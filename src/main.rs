22ded1aa1d3ac0fcf04757b97cf4e48467f71f5d
Bastien Léonard <bastien.leonard@gmail.com>
2022-12-04 21:15:35 +0100

    Solve day 2

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,131 @@ fn ex1_2() -> i32 {
     biggest3.iter().sum()
 }
 
+enum Choice {
+    Rock,
+    Paper,
+    Scissors
+}
+
+struct Prediction {
+    his_choice: Choice,
+    my_choice: Choice
+}
+
+enum GameResult {
+    Win,
+    Loss,
+    Draw
+}
+
+impl GameResult {
+    fn points(self) -> i32 {
+        match self {
+            GameResult::Win => 6,
+            GameResult::Draw => 3,
+            GameResult::Loss => 0
+        }
+    }
+}
+
+fn result_points(prediction: &Prediction) -> i32 {
+    let result = match prediction.my_choice {
+        Choice::Rock => match prediction.his_choice {
+            Choice::Rock => GameResult::Draw,
+            Choice::Paper => GameResult::Loss,
+            Choice::Scissors => GameResult::Win
+        },
+        Choice::Paper => match prediction.his_choice {
+            Choice::Rock => GameResult::Win,
+            Choice::Paper => GameResult::Draw,
+            Choice::Scissors => GameResult::Loss
+        },
+        Choice::Scissors => match prediction.his_choice {
+            Choice::Rock => GameResult::Loss,
+            Choice::Paper => GameResult::Win,
+            Choice::Scissors => GameResult::Draw
+        }
+    };
+
+    result.points()
+}
+
+fn choice_points(choice: &Choice) -> i32 {
+    match choice {
+        Choice::Rock => 1,
+        Choice::Paper => 2,
+        Choice::Scissors => 3
+    }
+}
+
+fn ex2_1() -> i32 {
+    let file = std::fs::File::open("input2.txt").unwrap();
+    let lines = std::io::BufReader::new(file).lines();
+    lines.map(
+        |result| {
+            let line = result.unwrap();
+            let line_bytes = line.as_bytes();
+            let line_items = (line_bytes[0] as char, line_bytes[2] as char);
+            let prediction = Prediction {
+                his_choice: match line_items.0 {
+                    'A' => Choice::Rock,
+                    'B' => Choice::Paper,
+                    'C' => Choice::Scissors,
+                    _ => unreachable!()
+                },
+                my_choice: match line_items.1 {
+                    'X' => Choice::Rock,
+                    'Y' => Choice::Paper,
+                    'Z' => Choice::Scissors,
+                    _ => unreachable!()
+                }
+            };
+            result_points(&prediction) + choice_points(&prediction.my_choice)
+        }
+    ).sum()
+}
+
+fn ex2_2() -> i32 {
+    let file = std::fs::File::open("input2.txt").unwrap();
+    let lines = std::io::BufReader::new(file).lines();
+    lines.map(
+        |result| {
+            let line = result.unwrap();
+            let line_bytes = line.as_bytes();
+            let line_items = (line_bytes[0] as char, line_bytes[2] as char);
+            let his_choice = match line_items.0 {
+                'A' => Choice::Rock,
+                'B' => Choice::Paper,
+                'C' => Choice::Scissors,
+                _ => unreachable!()
+            };
+            let game_result = match line_items.1 {
+                'X' => GameResult::Loss,
+                'Y' => GameResult::Draw,
+                'Z' => GameResult::Win,
+                _ => unreachable!()
+            };
+            let my_choice = match game_result {
+                GameResult::Win => match his_choice {
+                    Choice::Rock => Choice::Paper,
+                    Choice::Paper => Choice::Scissors,
+                    Choice::Scissors => Choice::Rock
+                },
+                GameResult::Loss => match his_choice {
+                    Choice::Rock => Choice::Scissors,
+                    Choice::Paper => Choice::Rock,
+                    Choice::Scissors => Choice::Paper
+                },
+                GameResult::Draw => his_choice
+            };
+            game_result.points() + choice_points(&my_choice)
+        }
+    ).sum()
+}
+
 fn main() {
     assert!(ex1_1() == 65912);
     assert!(ex1_2() == 195625);
+    assert!(ex2_1() == 13675);
+    assert!(ex2_2() == 14184);
 }
