bebb5a8869831b64c4f06470a2bc8f9c70eb78da
Bastien Léonard <bastien.leonard@gmail.com>
2022-12-07 18:47:52 +0100

    Solve day 4

---
+++ src/ex4.rs
@@ -0,0 +1,47 @@
+use std::io::BufRead;
+use std::ops::RangeInclusive;
+
+pub fn ex4_1() -> i32 {
+    let file = std::fs::File::open("input4.txt").unwrap();
+    std::io::BufReader::new(file).lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let groups: Vec<&str> = line.split(',').collect();
+            assert!(groups.len() == 2);
+            let first = parse_range(groups[0]);
+            let second = parse_range(groups[1]);
+            one_fully_contains(first, second) as i32
+        }).sum()
+}
+
+fn parse_range(s: &str) -> RangeInclusive<i32> {
+    let parts: Vec<&str> = s.split('-').collect();
+    assert!(parts.len() == 2);
+    let start: i32 = parts[0].parse::<i32>().unwrap();
+    let end: i32 = parts[1].parse::<i32>().unwrap();
+    let range = RangeInclusive::new(start, end);
+    assert!(range.end() >= range.start());
+    range
+}
+
+fn one_fully_contains(
+    first: RangeInclusive<i32>,
+    second: RangeInclusive<i32>
+) -> bool {
+    (first.start() <= second.start() && first.end() >= second.end())
+        || (second.start() <= first.start() && second.end() >= first.end())
+}
+
+pub fn ex4_2() -> i32 {
+    let file = std::fs::File::open("input4.txt").unwrap();
+    std::io::BufReader::new(file).lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let groups: Vec<&str> = line.split(',').collect();
+            assert!(groups.len() == 2);
+            let mut first = parse_range(groups[0]);
+            let second = parse_range(groups[1]);
+            first.any(|i| second.contains(&i)) as i32
+        })
+        .sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod ex1;
 pub mod ex2;
 pub mod ex3;
+pub mod ex4;
 
 fn main() {
     assert!(ex1::ex1_1() == 65912);
@@ -9,4 +10,6 @@ fn main() {
     assert!(ex2::ex2_2() == 14184);
     assert!(ex3::ex3_1() == 7568);
     assert!(ex3::ex3_2() == 2780);
+    assert!(ex4::ex4_1() == 441);
+    assert!(ex4::ex4_2() == 861);
 }
